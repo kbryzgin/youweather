107179895b0c34f9b828dfb81e2ab92ee1dd6e2d
kbryzgin <dev@bryzgin.ru>
2026-09-24 14:15:20 +0700

    feat: add json parser interface

--- include/parser.h
+++ include/parser.h
@@ -1,3 +1,21 @@
 /*
- * json parser interface
+ * parser.h - json-parsing core
  */
+
+ #pragma once
+
+typedef struct {
+    char temp_min[8];
+    char temp_max[8];
+    char wind_speed[12];
+    char wind_direction[8];
+    char description[64];
+} WeatherData;
+
+typedef enum {
+    PARSER_OK = 0,
+    PARSER_ERROR_INVALID_JSON,
+    PARSER_ERROR_MISSING_FIELDS
+} ParserStatus;
+
+int parse_json(const char* json_str, WeatherData* output);
